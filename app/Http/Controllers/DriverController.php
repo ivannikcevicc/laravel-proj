8a3b71fa6d6240eddfec12ac2c32c5124bfc5f7f
unknown <nikcevici13@gmail.com>
2024-11-10 23:01:48 +0100

    added trips

---
+++ app/Http/Controllers/DriverController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DriverController extends Controller
+{
+    public function show(Request $request)
+    {
+        //return back the user and associated driver model
+        $user = $request->user();
+        $user->load('driver');
+
+        return $user;
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'year' => 'required|numeric|min:2010|max:2025',
+            'make' => 'required',
+            'model' => 'required',
+            'color' => 'required|alpha',
+            'license_plate' => 'required',
+            'name' => 'required',
+        ]);
+        $user = $request->user();
+        $user->update($request->only('name'));
+
+        //create or update a driver associated with the user
+
+        $user->driver()->updateOrCreate($request->only('year', 'make', 'model', 'color', 'license_plate'));
+        $user->load('driver');
+        return $user;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\DriverController;
 use Illuminate\Support\Facades\Route; // Add this line to import Route
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TripController;
+use Illuminate\Http\Request;
 
 // Define your routes
 Route::post('/login', [LoginController::class, 'submit']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
+
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+  Route::get('/driver', [DriverController::class, 'show']);
+  Route::post('/driver', [DriverController::class, 'update']);
+  Route::post('/trip', [TripController::class, 'store']);
+  Route::get('/trip/{trip}', [TripController::class, 'show']);
+  Route::get('/user', function (Request $request) {
+    return $request->user();
+  });
+});
